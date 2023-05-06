278ff61a7c1bad2d714f76cba471915f8d60d760
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-07 00:32:12 +0700

    mobilenih

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -3,10 +3,6 @@ import axios from "axios";
 import Link from "next/link";
 import { useState } from "react";
 
-export const metadata = {
-  title: "Signup",
-};
-
 
 async function SubmitRegister(data){
     const namas = data.nama
@@ -56,6 +52,7 @@ export default function Signup() {
 
   return (
     <>
+    <title>Sign up</title>
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transhtmlForm -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
